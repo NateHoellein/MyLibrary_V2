8e41788ee9d651459374987bf220437d01755948
Nate Hoellein <natehoellein@gmail.com>
2013-07-08 07:39:26 -0400

    test for schema Title

--- spec/SchemaTests.js
+++ spec/SchemaTests.js
@@ -4,7 +4,18 @@ bookSchema = require('../lib/MyLibrarySchema');
 
 
 describe('My Library Schema',function() {
-    it('has the book info layout',function() {
-     var librarySchema = bookSchema(); 
+  var librarySchema;
+  before(function() {
+    librarySchema = bookSchema();
+    
     });
+
+  it('has the book info layout',function() {
+    verifySchemaProperty('Title','String');
+  });
+
+  var verifySchemaProperty = function(property, type) {
+    librarySchema.paths.hasOwnProperty(property);
+    librarySchema.paths[property].instance.should.equal(type);
+  };
 });
