fd186c51b9a0012a17bd6c4f2e34e2b09ffc0350
Nate Hoellein <natehoellein@gmail.com>
2013-07-10 20:19:15 -0400

    Added route for put

--- routes/library.js
+++ routes/library.js
@@ -2,11 +2,15 @@
 module.exports = function(app) {
   app.get('/api/Library',function(req, res) {
     res.set({'Content-type':'application/json'});
-    res.send({'Hello':'Boo'});
     res.send(200);
   });
 
   app.post('/api/Library',function(req, res) {
+    res.set({'Content-type':'applicaiton/json'});
+    res.send(201);
+  });
+
+  app.put('/api/Library',function(req, res) {
     res.set({'Content-type':'applicaiton/json'});
     res.send(201);
   });
