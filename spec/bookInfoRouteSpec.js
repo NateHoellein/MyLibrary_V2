05c9f7f17b86bcbe3fe18befa97e91e28c6ba906
Nate Hoellein <natehoellein@gmail.com>
2013-07-18 07:06:58 -0400

    Added spec for bookinfo routing

---
+++ spec/bookInfoRouteSpec.js
@@ -0,0 +1,26 @@
+var should = require('should'),
+  mocha = require('mocha'),
+  request = require('supertest'),
+  express = require('express'),
+  app = require('../app'),
+  routes = require('../routes'),
+  sinon = require('sinon'),
+  libraryController = require('../controllers/librarycontroller.js'); 
+
+describe('BookInfo Routes', function() {
+  it('get\'s a books details; /api/BookInfo',function(done) {
+    app = express();
+    routes(app);
+    request(app)
+      .get('/api/BookInfo')
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+      .end(function(err, response) {
+        if(err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+
+});
