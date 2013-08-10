02bdcbc448e593a39edd00c959fea3dacbf4735a
Nate Hoellein <natehoellein@gmail.com>
2013-08-10 09:42:23 -0400

    refactored a little

--- spec/bookInfoRouteSpec.js
+++ spec/bookInfoRouteSpec.js
@@ -10,9 +10,16 @@ var should = require('should'),
 describe('BookInfo Routes', function() {
   var infoController = new bookinfoController();
   var bookSpy = sinon.stub(infoController, 'getbookinfo').returns({Title:'I found you'});
-  it('get\'s a books details; /api/BookInfo',function(done) {
+
+  before(function() {
     app = express();
     routes(app,infoController);
+  });
+
+  after(function() {
+
+  });
+  it('get\'s a books details; /api/BookInfo',function(done) {
     request(app)
       .get('/api/BookInfo/some-isbn-number')
       .expect(200)
@@ -22,7 +29,7 @@ describe('BookInfo Routes', function() {
           return done(err);
         }
         infoController.getbookinfo.calledOnce.should.be.true; 
-        infoController.getbookinfo.calledWith('some-isbn-number').should.be.true;
+        infoController.getbookinfo.calledWith('some-isbn-number').should.be.true; 
         response.body.Title.should.be.equal('I found you');
         done();
       });
